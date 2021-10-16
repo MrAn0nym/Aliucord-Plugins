4ca276269c9a49c89b8e5aee52aba35e02879ae1
MrAn0nym <63542658+MrAn0nym@users.noreply.github.com>
2021-10-16 18:44:22 +0200

    Changelog for Timestamps and BetterDashless no longer broken

--- BetterDashless/build.gradle.kts
+++ BetterDashless/build.gradle.kts
@@ -2,8 +2,8 @@ version = "1.0.0"
 description = "Replaces dashes in channel names with spaces."
 aliucord {
     changelog.set("""
-        #1.0.0
-        *Update to Aliucord 96.3
+        # 1.0.0
+        * Update to Aliucord 96.3
     """.trimIndent())
 
     author("Nat", 156990761366192128L)
--- Timestamps/build.gradle.kts
+++ Timestamps/build.gradle.kts
@@ -2,14 +2,14 @@ version = "1.0.3"
 description = "Generates unix timestamps for discord and copies it to the clipboard."
 aliucord {
     changelog.set("""
-        #1.0.3
-        *Update to Aliucord 96.3
-        *Autocomplete for timezones
+        # 1.0.3
+        * Update to Aliucord 96.3
+        * Autocomplete for timezones
         
-        #1.0.2
-        *Uhh… Probably nothing, i don't remember xD
+        # 1.0.2
+        * Uhh… Probably nothing, i don't remember xD
         
-        #1.0.0
-        *First release
+        # 1.0.0
+        * First release
     """.trimIndent())
 }
