340c997ef1bddb2e11dd7ad38d5eba9769e254e7
MrAn0nym <63542658+MrAn0nym@users.noreply.github.com>
2021-10-16 19:59:58 +0200

    Update build.gradle.kts

--- Timestamps/build.gradle.kts
+++ Timestamps/build.gradle.kts
@@ -1,7 +1,13 @@
-version = "1.0.3"
+version = "1.0.5"
 description = "Generates unix timestamps for discord and copies it to the clipboard."
 aliucord {
     changelog.set("""
+        # 1.0.5
+        * See below
+        
+        # 1.0.4
+        * Migrate from the old repo to the new one
+        
         # 1.0.3
         * Update to Aliucord 96.3
         * Autocomplete for timezones
