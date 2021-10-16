7453c23661da447eabb6e2fdc1eb7bf58321c9a3
MrAn0nym <63542658+MrAn0nym@users.noreply.github.com>
2021-10-16 17:55:44 +0200

    New BetterNSFWBypass patch method

--- BetterNSFWBypass/src/main/java/com/aliucord/plugins/BetterNSFWBypass.java
+++ BetterNSFWBypass/src/main/java/com/aliucord/plugins/BetterNSFWBypass.java
@@ -1,12 +1,3 @@
-/*
- * The methods I needed to patch are copied from https://github.com/swishs-client-mod-plugins/aliucord-plugins/blob/main/NSFWGateBypass/src/main/java/com/aliucord/plugins/NSFWGateBypass.java
- * Those parts are released under GPL3 (https://github.com/swishs-client-mod-plugins/aliucord-plugins/blob/main/LICENSE)
- * Copyright (c) 2021 Paige
- * Licensed under the GNU General Public License v3.0
- *
- * Everything else is created by me and falls under the repos license
- */
-
 package com.aliucord.plugins;
 
 import android.content.Context;
@@ -14,19 +5,17 @@ import com.aliucord.annotations.AliucordPlugin;
 import com.aliucord.entities.Plugin;
 import com.aliucord.patcher.PreHook;
 import com.discord.api.user.NsfwAllowance;
-import com.discord.models.user.MeUser;
+import com.discord.widgets.home.WidgetHomeModel;
 
 @AliucordPlugin
 @SuppressWarnings("unused")
 public class BetterNSFWBypass extends Plugin {
 
   @Override
-  public void start(Context context) throws NoSuchMethodException {
-    patcher.patch(MeUser.class.getDeclaredMethod("getNsfwAllowance"),
-        new PreHook(callFrame -> callFrame.setResult(
-            NsfwAllowance.ALLOWED)));
-    patcher.patch(MeUser.class.getDeclaredMethod("getHasBirthday"),
-        new PreHook(callFrame -> callFrame.setResult(true)));
+  public void start(Context context) throws NoSuchMethodException, NoSuchFieldException {
+    patcher.patch(WidgetHomeModel.class.getDeclaredMethod("getNsfwAllowed"),
+        new PreHook(callFrame -> callFrame
+            .setResult(NsfwAllowance.ALLOWED)));
   }
 
   @Override
