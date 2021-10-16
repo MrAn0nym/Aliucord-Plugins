a5ca7471ed0dba8ad8d47b54f3cc7e6b12453d67
MrAn0nym <63542658+MrAn0nym@users.noreply.github.com>
2021-10-16 18:36:35 +0200

    - Alwaysconfirm (explanation in the changelog)   - 1.0.1

--- BetterNSFWBypass/build.gradle.kts
+++ BetterNSFWBypass/build.gradle.kts
@@ -1,11 +1,13 @@
-version = "1.0.0"
+version = "1.0.1"
 description = "Access NSFW channels without verifying your age."
 aliucord {
 
     changelog.set("""
-        #1.0.0
-        *Update to Aliucord 96.3
+        # 1.0.1
+        * When opening nsfw channels you'll get asked if you wish to continue
+        * This is optional and can be disabled in the settings
+                
+        # 1.0.0
+        * Update to Aliucord 96.3
     """.trimIndent())
-
-    author("Swishilicous", 474322346937810955L)
 }
--- BetterNSFWBypass/src/main/java/com/aliucord/plugins/BetterNSFWBypass.java
+++ BetterNSFWBypass/src/main/java/com/aliucord/plugins/BetterNSFWBypass.java
@@ -1,25 +1,65 @@
 package com.aliucord.plugins;
 
 import android.content.Context;
+import android.os.Bundle;
+import android.view.View;
+import com.aliucord.Utils;
 import com.aliucord.annotations.AliucordPlugin;
+import com.aliucord.api.SettingsAPI;
 import com.aliucord.entities.Plugin;
+import com.aliucord.patcher.InsteadHook;
 import com.aliucord.patcher.PreHook;
+import com.aliucord.widgets.BottomSheet;
 import com.discord.api.user.NsfwAllowance;
+import com.discord.views.CheckedSetting;
+import com.discord.views.CheckedSetting.ViewType;
 import com.discord.widgets.home.WidgetHomeModel;
 
 @AliucordPlugin
 @SuppressWarnings("unused")
 public class BetterNSFWBypass extends Plugin {
 
+  public BetterNSFWBypass() {
+    settingsTab = new SettingsTab(BetterNSFWBypassSettings.class, SettingsTab.Type.BOTTOM_SHEET)
+        .withArgs(settings);
+  }
+
   @Override
   public void start(Context context) throws NoSuchMethodException, NoSuchFieldException {
     patcher.patch(WidgetHomeModel.class.getDeclaredMethod("getNsfwAllowed"),
         new PreHook(callFrame -> callFrame
             .setResult(NsfwAllowance.ALLOWED)));
+    patcher.patch(WidgetHomeModel.class.getDeclaredMethod("isNsfwUnConsented"),
+        (settings.getBool("Alwaysconfirm", true)) ? new PreHook(
+            callFrame -> callFrame.setResult(true)) : InsteadHook.DO_NOTHING);
   }
 
   @Override
   public void stop(Context context) {
     patcher.unpatchAll();
+  }
+
+  public static class BetterNSFWBypassSettings extends BottomSheet {
+
+    private final SettingsAPI settings;
+
+    public BetterNSFWBypassSettings(SettingsAPI settings) {
+      this.settings = settings;
+    }
+
+    public void onViewCreated(View view, Bundle bundle) {
+
+      super.onViewCreated(view, bundle);
+      CheckedSetting always_confirm = Utils
+          .createCheckedSetting(requireContext(), ViewType.SWITCH, "NSFW Warning",
+              "Displays a warning before showing the NSFW channel.");
+      always_confirm.setChecked(true);
+      always_confirm.setChecked(settings.getBool("Alwaysconfirm", true));
+      always_confirm.setOnCheckedListener(checked -> {
+        settings.setBool("Alwaysconfirm", checked);
+        Utils.showToast("Please restart Aliucord to apply");
+      });
+      addView(always_confirm);
+    }
   }
 }
