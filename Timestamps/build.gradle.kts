fd1d9e63b302eabac90d444893b7b449f8c74217
MrAn0nym <63542658+MrAn0nym@users.noreply.github.com>
2021-10-20 18:11:06 +0200

    Update build.gradle.kts

--- Timestamps/build.gradle.kts
+++ Timestamps/build.gradle.kts
@@ -1,7 +1,10 @@
-version = "1.0.5"
+version = "1.0.6"
 description = "Generates unix timestamps for discord and copies it to the clipboard."
 aliucord {
     changelog.set("""
+        # 1.0.6
+        * Fixed bug when not giving args for the month/hour.
+        
         # 1.0.5
         * See below
         
