272129835803e0b271b0f84e30133b64fff743c1
MrAn0nym <63542658+MrAn0nym@users.noreply.github.com>
2021-10-29 19:24:44 +0200

    Rewrite

--- BetterDashless/build.gradle.kts
+++ BetterDashless/build.gradle.kts
@@ -1,10 +1,12 @@
-version = "1.0.0"
+version = "1.0.1"
 description = "Replaces dashes in channel names with spaces."
 aliucord {
     changelog.set("""
+        # 1.0.1
+        * Complete rewrite
+        * Channel search etc. is now working again
+        
         # 1.0.0
         * Update to Aliucord 96.3
     """.trimIndent())
-
-    author("Nat", 156990761366192128L)
 }
--- BetterDashless/src/main/java/com.aliucord.plugins/BetterDashless.java
+++ BetterDashless/src/main/java/com.aliucord.plugins/BetterDashless.java
@@ -8,23 +8,59 @@
 package com.aliucord.plugins;
 
 import android.content.Context;
+import android.widget.TextView;
+import com.aliucord.Utils;
 import com.aliucord.annotations.AliucordPlugin;
 import com.aliucord.entities.Plugin;
 import com.aliucord.patcher.Hook;
-import com.discord.api.channel.Channel;
+import com.aliucord.utils.ReflectUtils;
+import com.discord.databinding.WidgetChannelsListItemChannelBinding;
+import com.discord.databinding.WidgetHomeBinding;
+import com.discord.widgets.channels.list.WidgetChannelsListAdapter.ItemChannelText;
+import com.discord.widgets.channels.list.items.ChannelListItem;
+import com.discord.widgets.home.WidgetHome;
+import com.discord.widgets.home.WidgetHomeHeaderManager;
+import com.discord.widgets.home.WidgetHomeModel;
 
 @AliucordPlugin
 @SuppressWarnings("unused")
 public class BetterDashless extends Plugin {
-
-  @Override
-  public void start(Context context) throws NoSuchMethodException {
-    patcher.patch(Channel.class.getDeclaredMethod("m"), new Hook(
-        callFrame -> callFrame.setResult(callFrame.getResult().toString().replace("-", " "))));
-  }
-
-  @Override
-  public void stop(Context context) {
-    patcher.unpatchAll();
-  }
+	
+	@Override
+	public void start(Context context) {
+		
+		patcher.patch(ItemChannelText.class, "onConfigure",
+				new Class<?>[]{int.class, ChannelListItem.class}, new Hook(callFrame -> {
+					ItemChannelText _this = (ItemChannelText) callFrame.thisObject;
+					
+					WidgetChannelsListItemChannelBinding binding = null;
+					try {
+						binding = (WidgetChannelsListItemChannelBinding) ReflectUtils
+								.getField(_this, "binding");
+					} catch (NoSuchFieldException | IllegalAccessException e) {
+						e.printStackTrace();
+					}
+					
+					assert binding != null;
+					TextView channelName = binding.getRoot()
+							.findViewById(Utils.getResId("channels_item_channel_name", "id"));
+					
+					channelName.setText(channelName.getText().toString().replace("-", " "));
+				}));
+		
+		patcher.patch(WidgetHomeHeaderManager.class, "configure",
+				new Class<?>[]{WidgetHome.class, WidgetHomeModel.class, WidgetHomeBinding.class},
+				new Hook(callFrame -> {
+					WidgetHomeHeaderManager _this = (WidgetHomeHeaderManager) callFrame.thisObject;
+					WidgetHome widgetHome = (WidgetHome) callFrame.args[0];
+					
+					widgetHome.setActionBarTitle(
+							((WidgetHomeModel) callFrame.args[1]).getChannel().m().replace("-", " "));
+				}));
+	}
+	
+	@Override
+	public void stop(Context context) {
+		patcher.unpatchAll();
+	}
 }
