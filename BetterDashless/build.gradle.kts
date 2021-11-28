37d5552b05fee5b197224db620ba120067c43514
MrAn0nym <63542658+MrAn0nym@users.noreply.github.com>
2021-11-28 17:12:48 +0100

    Update build.gradle.kts

--- BetterDashless/build.gradle.kts
+++ BetterDashless/build.gradle.kts
@@ -1,7 +1,10 @@
-version = "1.0.1"
+version = "1.0.2"
 description = "Replaces dashes in channel names with spaces."
 aliucord {
     changelog.set("""
+        # 1.0.2
+        * Fixed incompatibility with TwemojiEverywhere
+        
         # 1.0.1
         * Complete rewrite
         * Channel search etc. is now working again
