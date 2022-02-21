f12db7f49b345268d3dd4410ec925f49b303f689
MrAn0nym <63542658+MrAn0nym@users.noreply.github.com>
2022-02-21 19:46:09 +0100

    [BetterNSFWBypass] Fucking fixed it, let's go - Fixes #4

--- BetterNSFWBypass/src/main/java/com/github/MrAn0nym/BetterNSFWBypass.java
+++ BetterNSFWBypass/src/main/java/com/github/MrAn0nym/BetterNSFWBypass.java
@@ -3,6 +3,7 @@ package com.github.MrAn0nym;
 import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewStub;
 import com.aliucord.Utils;
 import com.aliucord.annotations.AliucordPlugin;
 import com.aliucord.api.SettingsAPI;
@@ -10,11 +11,10 @@ import com.aliucord.entities.Plugin;
 import com.aliucord.patcher.InsteadHook;
 import com.aliucord.widgets.BottomSheet;
 import com.discord.api.user.NsfwAllowance;
-import com.discord.models.user.MeUser;
 import com.discord.views.CheckedSetting;
 import com.discord.views.CheckedSetting.ViewType;
-import com.discord.widgets.home.WidgetHomeHeaderManager;
-import com.discord.widgets.home.WidgetHomeModel;
+import com.discord.widgets.home.WidgetHomePanelNsfw;
+import kotlin.jvm.functions.Function1;
 
 @AliucordPlugin(/*requiresRestart = true*/)
 @SuppressWarnings("unused")
@@ -29,19 +29,7 @@ public class BetterNSFWBypass extends Plugin {
 	
 	@Override
 	public void start(Context context) throws NoSuchMethodException {
-		patcher.patch(MeUser.class.getDeclaredMethod("getHasBirthday"),
-				new InsteadHook(param -> true));
-		patcher.patch(WidgetHomeModel.class.getDeclaredMethod("getNsfwAllowed"),
-				new InsteadHook(param -> NsfwAllowance.ALLOWED));
-		patcher.patch(WidgetHomeModel.class.getDeclaredMethod("isNsfwUnConsented"),
-				new InsteadHook(param -> true));
-		patcher.patch(WidgetHomeModel.class.getDeclaredMethod("isChannelNsfw"),
-				new InsteadHook(param -> false));
-
-		/*if (settings.getBool(ALWAYSCONFIRM, true)) {
-			patcher.patch(WidgetHomeModel.class.getDeclaredMethod("isNsfwUnConsented"),
-					new InsteadHook(param -> true));
-		}*/
+		patcher.patch(WidgetHomePanelNsfw.class.getDeclaredMethod("toggleContainerVisibility", boolean.class, boolean.class, NsfwAllowance.class, ViewStub.class, Function1.class), InsteadHook.DO_NOTHING);
 	}
 	
 	@Override
@@ -65,12 +53,11 @@ public class BetterNSFWBypass extends Plugin {
 					.createCheckedSetting(requireContext(), ViewType.SWITCH, "NSFW Warning",
 							"Disabled for now - Displays a warning before showing the NSFW channel.");
 			alwaysConfirm.setChecked(settings.getBool(ALWAYSCONFIRM, true));
-			alwaysConfirm.setClickable(false);
-			alwaysConfirm.setEnabled(false);
-			/*alwaysConfirm.setOnCheckedListener(checked -> {
-				settings.setBool(ALWAYSCONFIRM, checked);
-				Utils.showToast("Please restart Aliucord to apply");
-			});*/
+			alwaysConfirm.setOnCheckedListener(checked -> {
+				alwaysConfirm.setChecked(false);
+				//settings.setBool(ALWAYSCONFIRM, checked);
+				//Utils.showToast("Please restart Aliucord to apply");
+			});
 			addView(alwaysConfirm);
 		}
 	}
